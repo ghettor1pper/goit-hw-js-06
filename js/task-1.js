72c0174e26c1a5ba003d678b407eba3715c2d550
ghettoripper <ap4ixba@gmail.com>
2024-03-04 20:02:17 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,39 +1,30 @@
-class Customer {
-  constructor(username, balance, discountRate, orders) {
-    this.username = username;
-    this.balance = balance;
-    this.discountRate = discountRate;
-    this.orders = orders;
-  }
-
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
   getBalance() {
     return this.balance;
-  }
-
-  getDiscountRate() {
-    return this.discountRate;
-  }
-
-  setDiscountRate(value) {
-    this.discountRate = value;
-  }
-
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
   getOrders() {
     return this.orders;
-  }
-
+  },
   addOrder(cost, order) {
-    this.balance -= cost - cost * this.discountRate;
+    this.balance -= cost - cost * this.discount;
     this.orders.push(order);
-  }
-}
-
-const customer = new Customer("Mango", 24000, 0.1, ["Burger", "Pizza", "Salad"]);
-
-customer.setDiscountRate(0.15);
-console.log(customer.getDiscountRate()); // 0.15
-
+  },
+  // Change code above this line
+};
+//
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
 customer.addOrder(5000, "Steak");
 console.log(customer.getBalance()); // 19750
-
 console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
